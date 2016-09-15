143c5221bcb597140146fa7466964929979ecb71
Florian Brucker <florian.brucker@mb.karlsruhe.de>
2016-09-15 14:34:14 +0200

    Parsing of 'Investitionsübersicht' tables.

--- export.py
+++ export.py
@@ -79,10 +79,12 @@ class Table(list):
     subclasses:
 
     - The entry point for parsing is ``_parse``, which is called by
-      ``__init__`` with the table data. It first calls
-      ``_parse_non_value_headers`` and ``_parse_value_headers`` before
-      using ``_parse_row`` to parse the individual rows and group them
-      into positions and their children.
+      ``__init__`` with the table data. It calls ``_parse_headers``
+      before using ``_parse_row`` to parse the individual rows and group
+      them into positions and their children.
+
+    - ``_parse_headers`` receives the first row of the table and passes
+      it on to ``_parse_non_value_headers`` before calling ``_parse_value_headers``
 
     - ``_parse_non_value_headers`` must be implemented by subclasses. It
       receives the first row of the data and must set
@@ -99,41 +101,45 @@ class Table(list):
       header and uses them to set ``self._types`` and ``self._years`` to
       lists containing the value column's types and years, respectively.
     '''
-
     def __init__(self, data):
         super(Table, self).__init__()
         self._parse(data)
 
-    def _parse_non_value_headers(self, header):
-        raise NotImplementedError('Must be implemented by subclass')
+    def _parse_meta_headers(self, header):
+        raise NotImplementedError('Must be implemented in subclass')
 
-    def _parse_value_headers(self, headers):
-        self._years = []
-        self._types = []
-        for cell in headers:
-            type, year, _ = split(cell, 2)
-            self._types.append(type.lower())
-            self._years.append(parse_int(year))
-        #print(self._types)
-        #print(self._years)
+    def _parse_value_headers(self, header):
+        self._value_columns = {}
+        num_meta = len(self._meta_columns)
+        for i, cell in enumerate(header[num_meta:], num_meta):
+            parts = cell.split()
+            if (len(parts) == 3) and (parts[2] == 'EUR'):
+                try:
+                    year = parse_int(parts[1])
+                except ValueError:
+                    # Not a year
+                    continue
+                self._value_columns[i] = (parts[0], year)
+
+    def _parse_headers(self, header):
+        self._parse_meta_headers(header)
+        self._parse_value_headers(header)
 
     def _parse_row(self, row):
         values = []
         record = {'values': values}
-        for i, (key, transform) in enumerate(self._non_value_columns):
+        for i, (key, transform) in self._meta_columns.iteritems():
             value = row[i]
             if transform:
                 value = transform(value)
             record[key] = value
-        for j, cell in enumerate(row[i + 1:]):
-            values.append({'type': self._types[j], 'year': self._years[j],
-                           'amount': parse_amount(cell)})
+        for i, (type, year) in self._value_columns.iteritems():
+            values.append({'type': type, 'year': year,
+                           'amount': parse_amount(row[i])})
         return record
 
     def _parse(self, data):
-        header = data[0]
-        self._parse_non_value_headers(header)
-        self._parse_value_headers(header[len(self._non_value_columns):])
+        self._parse_headers(data[0])
         position = None
         for row in data[2:]:  # The second row is part of the header
             record = self._parse_row(row)
@@ -153,14 +159,17 @@ class ErgebnishaushaltTable(Table):
 
     _KONTOGRUPPE_HEADER = 'Kto.\nGr.'
 
-    def _parse_non_value_headers(self, header):
-        self._non_value_columns = [('number', parse_int)]
+    def _parse_meta_headers(self, header):
+        self._meta_columns = {
+            0: ('number', parse_int),
+        }
         if header[1] == self._KONTOGRUPPE_HEADER:
-            self._non_value_columns.append(('kontogruppe', None))
-        self._non_value_columns.extend([
-            ('sign', None),
-            ('title', None),
-        ])
+            self._meta_columns[1] = ('kontogruppe', None)
+            offset = 1
+        else:
+            offset = 0
+        self._meta_columns[1 + offset] = ('sign', None)
+        self._meta_columns[2 + offset] = ('title', None)
 
     def _parse_row(self, row):
         record = super(ErgebnishaushaltTable, self)._parse_row(row)
@@ -172,37 +181,110 @@ class ErgebnishaushaltTable(Table):
 
 class FinanzhaushaltTable(Table):
 
-    def _parse_non_value_headers(self, header):
-        self._non_value_columns = [
-            ('number', parse_int),
-            ('sign', None),
-            ('title', None)
-        ]
+    def _parse_meta_headers(self, header):
+        self._meta_columns = {
+            0: ('number', parse_int),
+            1: ('sign', None),
+            2: ('title', None),
+        }
 
 
-def extract_table(table):
+class InvestitionsuebersichtTable(Table):
+
+    def _parse_meta_headers(self, header):
+        self._meta_columns = {
+            0: ('number', parse_int),
+            1: ('sign', None),
+            2: ('title', None),
+        }
+
+    def _parse(self, data):
+        self._parse_headers(data[0])
+        self._parse_body(data[2:])  # Second row is part of the header
+
+    def _parse_body(self, rows):
+        project = None
+        position = None
+        for row in rows:
+            if len(set(row)) == 1:
+                # Row consisting of a single merged cell containing the
+                # project ID and name.
+                parts = row[0].split(':', 1)
+                project = {
+                    'id': parts[0].strip(),
+                    'title': parts[1].strip(),
+                    'positions': [],
+                }
+                self.append(project)
+            else:
+                # Standard row
+                record = self._parse_row(row)
+                if record['number']:
+                    assert record['sign']
+                    record['children'] = []
+                    position = record
+                    project['positions'].append(position)
+                else:
+                    assert not record.pop('sign')
+                    assert not record.pop('kontogruppe', None)
+                    position['children'].append(record)
+
+    def append_data(self, data):
+        '''
+        Add more data for this table.
+
+        If a Investitionsübersicht contains multiple projects then each
+        project is exported into a separate Word table. Only the first
+        of these has a header and should be parsed by creating a new
+        ``InvestitionsuebersichtTable`` instance. The other, header-less
+        tables then can be added to that table via ``append_data``.
+        '''
+        self._parse_body(data)
+
+
+def extract_data(table):
     data = []
     for row in table.rows:
         data.append([cell.text.strip() for cell in row.cells])
     return data
 
 
-def convert_table(table):
-    data = extract_table(table)
-    if 'finanzhaushalt' in data[0][2]:
+def convert_data(data):
+    if 'finanzhaushalt' in data[0][2].lower():
         return FinanzhaushaltTable(data)
-    else:
+    elif 'investitionsübersicht' in data[0][2].lower():
+        return InvestitionsuebersichtTable(data)
+    elif ('ergebnishaushalt' in data[0][2].lower()
+            or 'ergebnishaushalt' in data[0][3].lower()):
         return ErgebnishaushaltTable(data)
+    else:
+        raise ValueError('Unknown table type.')
 
 
 if __name__ == '__main__':
     import sys
     from pprint import pprint
 
-    filename = sys.argv[1]
-    doc = Document(filename)
+    filenames = sys.argv[1:]
 
-    for table in doc.tables:
-        result = convert_table(table)
-        pprint(result)
+    tables = []
+    for filename in filenames:
+        print('Loading {}'.format(filename))
+        doc = Document(filename)
+        for table in doc.tables:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            data = extract_data(table)
+            try:
+                tables.append(convert_data(data))
+            except ValueError:
+                # Assume it's a sub-table of an Investitionsübersicht
+                tables[-1].append_data(data)
         print('')
+
+    for table in tables:
+        pprint(table)
+        print('')
+        print('-' * 70)
+        print('')
+
